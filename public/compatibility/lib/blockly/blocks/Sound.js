b301a65b44db95450cbea25bc6826ed08c3b5ff9
Armando Gonzalez <iam@armando.mx>
2018-02-20 22:10:57 -0600

    Added missing sensors

--- public/compatibility/lib/blockly/blocks/Sound.js
+++ public/compatibility/lib/blockly/blocks/Sound.js
@@ -1,7 +1,7 @@
-Blockly.Blocks.distance = {
-  init() {
+Blockly.Blocks.sound = {
+  init: function() {
     this.appendDummyInput()
-      .appendField('Sensor de Distancia')
+      .appendField('Sensor de Sonido')
       .appendField(
         new Blockly.FieldImage(
           'http://robotois.com/static/media/logo.b650e89d.svg',
@@ -12,14 +12,13 @@ Blockly.Blocks.distance = {
       );
     this.setOutput(true, 'Sensor');
     this.setColour(240);
-    this.setTooltip('Sensor de distancia');
-    this.setHelpUrl('');
+    this.setTooltip('Sensor de Sonido');
+    this.setHelpUrl('www.robotois.com');
   },
 };
-
-Blockly.JavaScript.distance = function generateCode(block) {
+Blockly.JavaScript.sound = function generateCode(block) {
   // TODO: Assemble JavaScript into code variable.
-  const code = 'distance[0]';
+  const code = 'sound[0]';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
